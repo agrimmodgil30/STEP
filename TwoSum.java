203cefd2b31bd208a1bc1566fcaeb39341e99bb5
agrimmodgil30 <agrimmodgil30@gmail.com>
2026-02-16 03:22:56 +0530

    Two numbers sum

---
+++ TwoSum.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class TwoSum{
+public static void main(String args[]){
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter two numbers");
+int n1=sc.nextInt();
+int n2=sc.nextInt();
+int sum=n1+n2;
+System.out.println("Sum = " + sum);
+}
+}
+
+
